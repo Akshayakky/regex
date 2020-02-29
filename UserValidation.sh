2c6318f385b0217641a7bc9caaf20057cfc349e8
akshayakky <akshaysmpt01@gmail.com>
2020-02-29 12:54:31 +0530

    ADDED : Last Name Validation

--- UserValidation.sh
+++ UserValidation.sh
@@ -3,10 +3,18 @@
 echo "Welcome To User Registration Problem "
 
 read -p "Enter First Name : " FIRSTNAME
+read -p "Enter Last Name : " LASTNAME
 
 pattern="^[A-Z][a-zA-Z]{2,}$"
 
 if [[ ! $FIRSTNAME =~ $pattern ]]
 then
 	echo "Invalid First Name!"
+fi
+
+pattern="^[A-Z][a-zA-Z]{2,}$"
+
+if [[ ! $LASTNAME =~ $pattern ]]
+then
+	echo "Invalid Last Name!"
 fi
