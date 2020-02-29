f01104cbca6d732af2e6005f61a172067bdee374
akshayakky <akshaysmpt01@gmail.com>
2020-02-29 17:18:25 +0530

    ADDED : Email Validation

--- UserValidation.sh
+++ UserValidation.sh
@@ -2,9 +2,12 @@
 
 echo "Welcome To User Registration Problem "
 
+#READING FIRSTNAME, LASTNAME AND EMAIL FROM USER
 read -p "Enter First Name : " FIRSTNAME
 read -p "Enter Last Name : " LASTNAME
+read -p "Enter Email : " EMAIL
 
+#PATTERN FOR FIRSTNAME
 pattern="^[A-Z][a-zA-Z]{2,}$"
 
 if [[ ! $FIRSTNAME =~ $pattern ]]
@@ -12,9 +15,18 @@ then
 	echo "Invalid First Name!"
 fi
 
+#PATTERN FOR LASTNAME
 pattern="^[A-Z][a-zA-Z]{2,}$"
 
 if [[ ! $LASTNAME =~ $pattern ]]
 then
 	echo "Invalid Last Name!"
+fi
+
+#PATTERN FOR EMAIL
+pattern="^[0-9A-Za-z]+([-_+.][0-9A-Za-z]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$"
+
+if [[ ! $EMAIL =~ $pattern ]]
+then
+	echo "Invalid Email!"
 fi
